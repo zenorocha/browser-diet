a2b9f0917612bcb8b8e1568c63cb6ec942e64e81
Raphael Amorim <rapha850@gmail.com>
2014-04-16 19:40:47 -0300

    make site.assets works

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     cssmin: {
       dist: {
-        src: ['out/en/assets/css/main.css'],
-        dest: 'out/en/assets/css/all.min.css'
+        src: ['out/assets/css/main.css'],
+        dest: 'out/assets/css/all.min.css'
       }
     }
   });
